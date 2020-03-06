ed0c4afb762a8ec2a1575b40676ae875888a1f39
Ido Kanner <idok@linesip.com>
2020-03-06 15:38:17 +0200

    more documentation

--- ast_types.go
+++ ast_types.go
@@ -8,20 +8,20 @@ type ASTSymbolType int
 
 // ASTChar holds information regarding a given char
 type ASTChar struct {
-	Pos      int
-	Char     []byte
-	CharType ASTCharType
+	Pos      int         // Position of the "char". Rune position, rather than byte position
+	Char     []byte      // The character that can be a multi-byte
+	CharType ASTCharType // The type/family of char that was found
 }
 
 // ASTSymbol holds information about a block
 type ASTSymbol struct {
-	Chars      []ASTChar
-	SymbolType ASTSymbolType
+	Chars      []ASTChar     // A slice of ASTChar that generate a symbol
+	SymbolType ASTSymbolType // The type of symbol that was found
 }
 
 // AST holds information about each char and it's requirements
 type AST struct {
-	Symbols []ASTSymbol
+	Symbols []ASTSymbol // A list of symbols that generate a document
 }
 
 // The type of char that currently processed
