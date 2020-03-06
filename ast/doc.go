f496a000a6105abe40670760b792c98f39e4af91
Ido Kanner <idok@linesip.com>
2020-03-06 16:02:50 +0200

    work on documentation while creating code :)

---
+++ ast/doc.go
@@ -0,0 +1,4 @@
+/*Package ast parse a given text and convert it to a list of symbols that can be
+translated in an easier manner.
+*/
+package ast
--- ast/parser.go
+++ ast/parser.go
@@ -1,6 +1,3 @@
-/*Package ast parse a given text and convert it to a list of symbols that can be
-translated in an easier manner.
-*/
 package ast
 
 const (
