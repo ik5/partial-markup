69b6d0278b85c474d276320fb8e63c8ce92a5d29
Ido Kanner <idok@linesip.com>
2020-03-06 16:07:55 +0200

    fixed extra space

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-/* Package partialmarkup takes a given string (document) that can
+/*Package partialmarkup takes a given string (document) that can
 * contain at markup tags (HTML Style) such as <div></div>, and convert
 * each element to a readable tree that can be used by an application.
  */
