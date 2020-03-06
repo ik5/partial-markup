d6c9323b652e53b4d50e650fda45f3685b018812
Ido Kanner <idok@linesip.com>
2020-03-06 16:08:50 +0200

    styling the documentation

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
-/*Package partialmarkup takes a given string (document) that can
-* contain at markup tags (HTML Style) such as <div></div>, and convert
-* each element to a readable tree that can be used by an application.
- */
+/*Package partialmarkup takes a given string (document) that can contain at
+markup tags (HTML Style) such as <div></div>, and convert each element to a
+readable tree that can be used by an application.
+*/
 package partialmarkup
